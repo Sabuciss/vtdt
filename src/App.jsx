51cc8c3e974ad39158d59cacced39c1e996ec825
unknown <ipb23.s.brikmane@vtdt.edu.lv>
2025-06-05 14:27:22 +0300

    man amīļāka vieta

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,36 @@ import photo from "./assets/IMG_6760.JPG";
 import "./App.css";
 
 export default function App() {
+  const [spin, setSpin] = useState(false);
   const [show, setShow] = useState(false);
 
   return (
     <div className="img-center">
       <div className="img-wrap">
-        <img src={photo} alt="Mana vieta VTDT" className="photo" />
+        <img
+          src={photo}
+          alt="Mana vieta VTDT"
+          className={`photo ${spin ? "spin" : show ? "photo-effect" : ""}`}
+          onAnimationEnd={() => {
+            setSpin(false);
+            setShow(true);
+          }}
+        />
 
         <button
           className="hotspot-btn"
-          onClick={() => setShow(!show)}
+          onClick={() => {
+            setShow(false);
+            setSpin(true);
+          }}
         >
           Nospied mani
         </button>
 
         {show && (
           <div className="info">
-            Šī ir man mīļākā vieta VTDT, kad ir labs laiks, jo te var vērot
-            smukas mašīnas/močus, kas brauc garām!(aka soliņi)
+            Šī ir man mīļākā vieta VTDT, jo te var vērot
+            smukas mašīnas/močus, kas brauc garām! (aka soliņi)
           </div>
         )}
       </div>
